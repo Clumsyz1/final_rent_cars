41e6177d523fead704d34a501d1600211dbe3911
Clumsyz1 <Supathat27@gmail.com>
2025-04-16 13:35:08 +0700

    Add Datepicler

--- app/page.js
+++ app/page.js
@@ -1,10 +1,25 @@
+"use client";
+
+import { Grid, Container, Typography } from "@mui/material";
 import MyAppBar from "@/components/Appbar";
+import DateRangePicker from "@/components/DateRangePicker";
 
 export default function Page() {
   return (
     <div>
       <MyAppBar />
-      <h2>lorem</h2>
+      <Container maxWidth="md">
+        <Grid container justifyContent="center" mt={8}>
+          <Grid item xs={12}>
+            <Typography variant="h4" align="center" gutterBottom>
+              📅 เลือกช่วงวันที่ที่ต้องการเช่ารถ
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={10} md={8}>
+            <DateRangePicker />
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
